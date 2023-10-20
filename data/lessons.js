f0c7727d6eea3b5765d83c0f2db5868391ca9105
Crusader <abdullah.mudathir@isslng.com>
2023-10-20 19:36:33 +0100

    feat: added the data in lesson.js

---
+++ data/lessons.js
@@ -0,0 +1,45 @@
+const lessons = [
+  {
+    id: 1,
+    subject: "Maths",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    availableSpace: 0,
+  },
+  {
+    id: 2,
+    subject: "English",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    availableSpace: 0,
+  },
+  {
+    id: 3,
+    subject: "Physics",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    availableSpace: 0,
+  },
+  {
+    id: 4,
+    subject: "Geography",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    availableSpace: 0,
+  },
+  {
+    id: 5,
+    subject: "Orange",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    availableSpace: 0,
+  },
+];
+
+// export default afterSchoolActivity;
+export default lessons;
